b108fd4221c30aacad5a2c61f60bb573bed73c33
Quinn Krug <quinn.krug@gmail.com>
2018-08-29 13:43:29 -0600

    rooms_from_catagory_working

--- lib/house.rb
+++ lib/house.rb
@@ -13,10 +13,9 @@ class House
     @rooms << room
   end
 
-  def rooms_from_category(room)
+  def rooms_from_category(catagory)
     @rooms.select do |room|
-      room.catagory
-      binding.pry
+      room.catagory == catagory
     end
   end
 
