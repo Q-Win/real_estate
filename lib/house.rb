6c9a880af8e22d040085cafdcc188f3b663af491
Quinn Krug <quinn.krug@gmail.com>
2018-08-29 13:31:17 -0600

    working on rooms_from_catagory method

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,23 @@
 
 class House
 
-  attr_reader :price, :address 
+  attr_reader :price, :address, :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def rooms_from_category(room)
+    @rooms.select do |room|
+      room.catagory
+      binding.pry
+    end
   end
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -2,6 +2,7 @@ require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/house'
+require './lib/room'
 
 
 class HouseTest < Minitest::Test
@@ -10,6 +11,39 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
 
     assert_instance_of House, house
+  end
+
+  def test_it_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_we_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1,room_2], house.rooms
+  end
+
+  def test_we_can_collect_rooms_by_catagory
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1,room_2], house.rooms_from_category(:bedroom)
   end
 
 end
